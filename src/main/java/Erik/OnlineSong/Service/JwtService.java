ebcdaa095aecab9fbb7d2f3786efa30f9d0b63df
Erik <bako.erik.dominik@gmail.com>
2024-08-04 21:06:08 +0200

    USerDetailsImp and JwtService added

---
+++ src/main/java/Erik/OnlineSong/Service/JwtService.java
@@ -0,0 +1,60 @@
+package Erik.OnlineSong.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Service;
+
+import Erik.OnlineSong.Model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "e058f473514298481eaf470f6d3176ad1a067bcb53829e0cbd6b83cc284e1955";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token, User user) {
+        String username = extractUsername(token);
+        return username.equals(user.getUsername());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(User user) {
+        String token = Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
+                .signWith(getSigninKey())
+                .compact();
+
+        return token;
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
---
+++ src/main/java/Erik/OnlineSong/Service/UserDetailsImp.java
@@ -0,0 +1,25 @@
+package Erik.OnlineSong.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import Erik.OnlineSong.Repository.UserRepository;
+
+@Service
+public class UserDetailsImp implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserDetailsImp(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
