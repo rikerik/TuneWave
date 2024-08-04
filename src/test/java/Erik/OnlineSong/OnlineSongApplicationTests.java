82d56a937f7e01029f204d93d0e7c72bab14628e
Erik <bako.erik.dominik@gmail.com>
2024-08-04 19:07:16 +0200

    init commit

---
+++ src/test/java/Erik/OnlineSong/OnlineSongApplicationTests.java
@@ -0,0 +1,13 @@
+package Erik.OnlineSong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
