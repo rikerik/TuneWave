7a60b1f3cf2ae14bae33666648ebca2ac43fc7c9
Erik <bako.erik.dominik@gmail.com>
2024-08-05 09:18:37 +0200

    SecurityConfig is added

--- src/main/java/Erik/OnlineSong/Config/SecurityConfig.java
+++ src/main/java/Erik/OnlineSong/Config/SecurityConfig.java
@@ -1,10 +1,68 @@
 package Erik.OnlineSong.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import Erik.OnlineSong.Filter.JwtAuthenticationFilter;
+import Erik.OnlineSong.Service.UserDetailsServiceImp;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsServiceImp userDetailsServiceImp;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(UserDetailsServiceImp userDetailsServiceImp,
+            JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userDetailsServiceImp = userDetailsServiceImp;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        /*
+         * Disabling CSRF protection because the app uses jwt which is not susceptible
+         * to CSRF attacks
+         * Allows unauthenticated access to login and register endpoints
+         * Specifying which userDetailsService to use
+         * Configuring the session management to be stateless, because jwt is used for
+         * authentication
+         * No http session is created
+         */
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req -> req.requestMatchers("/login/**", "/register/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .userDetailsService(userDetailsServiceImp)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+
+    }
 }
