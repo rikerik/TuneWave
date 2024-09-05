7000e4d8524fb5f8d16b0da11bc9e2233abbd488
Erik <bako.erik.dominik@gmail.com>
2024-09-05 22:51:40 +0200

    Changed image for README

--- song-front/src/components/common/Navbar.jsx
+++ song-front/src/components/common/Navbar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
     <Navbar bg="dark" variant="dark" expand="lg" className="navbar">
       <Navbar.Brand className="ms-3">TuneWave</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/users">
-          Users
-        </Nav.Link>
+        {/*    <Nav.Link as={Link} to="/users">
+         Users
+       </Nav.Link> */}
         <Nav.Link as={Link} to="/home">
           Home Page
         </Nav.Link>
