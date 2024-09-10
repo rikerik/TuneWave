9a56952017f3fabed4727a90189c9e1af46a05c5
Erik <bako.erik.dominik@gmail.com>
2024-09-10 14:19:39 +0200

    Updated navbar and sidebar

--- song-front/src/components/common/Navbar.jsx
+++ song-front/src/components/common/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LogoutButton from "./Logout";
 import "../../styles/NavBar.css";
@@ -8,6 +8,28 @@ import "../../styles/NavBar.css";
 const logoSrc = "/logo.png";
 
 const NavBar = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const searchRef = useRef(null);
+
+  // Toggle search bar visibility when "Browse" is clicked
+  const handleBrowseClick = () => {
+    setShowSearch((prev) => !prev);
+  };
+
+  // Detect clicks outside of the search bar to close it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setShowSearch(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [searchRef]);
+
   return (
     <Navbar
       bg="dark"
@@ -22,13 +44,28 @@ const NavBar = () => {
       </Navbar.Brand>
 
       {/* Nav links */}
-      <Nav className="mr-auto">
-        {/* <Nav.Link as={Link} to="/users">
-          Users
-        </Nav.Link> */}
-        <Nav.Link as={Link} to="/home" className="nav-link-large ms-3">
-          Home Page
+      <Nav className="mr-auto d-flex align-items-center">
+        {/* Browse button */}
+        <Nav.Link
+          as={Link}
+          to="/home"
+          className="nav-link-large ms-3"
+          onClick={handleBrowseClick}
+        >
+          Browse
         </Nav.Link>
+
+        {/* Conditional rendering of the search bar with animation */}
+        <div
+          ref={searchRef}
+          className={`search-container ms-3 ${showSearch ? "show" : "hide"}`}
+        >
+          {showSearch && (
+            <Form className="d-flex">
+              <FormControl type="text" className="me-2 search-bar" />
+            </Form>
+          )}
+        </div>
       </Nav>
 
       <Nav className="ms-auto">
--- song-front/src/components/common/SideBar.jsx
+++ song-front/src/components/common/SideBar.jsx
@@ -10,8 +10,8 @@ const Sidebar = ({ className }) => {
         <hr className="my-3 sidebar-divider" />
 
         {/* Navigation links */}
-        <Nav.Link as={Link} to="/browse" className="nav-link nav-link-orange">
-          Browse
+        <Nav.Link as={Link} to="/home" className="nav-link nav-link-orange">
+          Home Page
         </Nav.Link>
         <Nav.Link as={Link} to="/library" className="nav-link nav-link-orange">
           Library
