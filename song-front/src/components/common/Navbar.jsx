2864f9974a0f857032ab92ea50a15f39428396f2
Erik <bako.erik.dominik@gmail.com>
2024-09-11 22:34:05 +0200

    Navbar update

--- song-front/src/components/common/Navbar.jsx
+++ song-front/src/components/common/Navbar.jsx
@@ -1,35 +1,12 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import LogoutButton from "./Logout";
-import "../../styles/NavBar.css";
+import "../../styles/NavBar.css"; // Assuming this is where you store your CSS
 
 // Logo path
 const logoSrc = "/logo.png";
 
 const NavBar = () => {
-  const [showSearch, setShowSearch] = useState(false);
-  const searchRef = useRef(null);
-
-  // Toggle search bar visibility when "Browse" is clicked
-  const handleBrowseClick = () => {
-    setShowSearch((prev) => !prev);
-  };
-
-  // Detect clicks outside of the search bar to close it
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (searchRef.current && !searchRef.current.contains(event.target)) {
-        setShowSearch(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [searchRef]);
-
   return (
     <Navbar
       bg="dark"
@@ -44,27 +21,15 @@ const NavBar = () => {
       </Navbar.Brand>
 
       {/* Nav links */}
-      <Nav className="mr-auto d-flex align-items-center">
-        {/* Browse button */}
-        <Nav.Link
-          as={Link}
-          to="/home"
-          className="nav-link-large ms-3"
-          onClick={handleBrowseClick}
-        >
-          Browse
-        </Nav.Link>
-
-        {/* Conditional rendering of the search bar with animation */}
-        <div
-          ref={searchRef}
-          className={`search-container ms-3 ${showSearch ? "show" : "hide"}`}
-        >
-          {showSearch && (
-            <Form className="d-flex">
-              <FormControl type="text" className="me-2 search-bar" />
-            </Form>
-          )}
+      <Nav className="mx-auto d-flex justify-content-center align-items-center">
+        <div className="search-container">
+          <Form className="d-flex">
+            <FormControl
+              type="text"
+              className="me-2 search-bar"
+              placeholder="Search for tracks"
+            />
+          </Form>
         </div>
       </Nav>
 
