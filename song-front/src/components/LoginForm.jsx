e130f00a5cf93bae1ac0bc27d71cacfa6497bab0
Erik <bako.erik.dominik@gmail.com>
2024-09-30 23:27:47 +0200

    Forms are better looking

--- song-front/src/components/LoginForm.jsx
+++ song-front/src/components/LoginForm.jsx
@@ -1,79 +1,78 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../services/AuthService";
+import "../styles/Form.css";
 
 // Component for handling the user login form
 const LoginForm = () => {
-  // State hooks for form fields and error message
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(""); // State for error message
+  const [error, setError] = useState("");
 
-  // Hook for navigation
   const navigate = useNavigate();
 
-  // Handle form submission
   const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
+    event.preventDefault();
 
     try {
-      // Call the login service function with form data
       const response = await login({ username, password });
 
       if (response.status === 200) {
-        // On successful login, save the token to session storage
         const { token } = response.data;
         sessionStorage.setItem("token", token);
-        navigate("/home"); // Redirect to the user list page upon successful login
+        navigate("/home");
       } else {
-        // Handle non-successful status codes
-        setError("Invalid username or password"); // Set error message
+        setError("Invalid username or password");
       }
     } catch (error) {
       console.error("An error occurred during login:", error);
-      setError("An error occurred during login. Please try again."); // Set error message
+      setError("An error occurred during login. Please try again.");
     }
   };
 
   return (
-    <div className="container mt-5 w-25">
-      <h2 className="mb-4">Login</h2>
-      <form onSubmit={handleSubmit}>
-        {error && (
-          <div className="alert alert-danger" role="alert">
-            {error}
+    <div className="form-container">
+      <div className="card shadow p-4">
+        <h2 className="mb-4 text-center">Login</h2>
+        <form onSubmit={handleSubmit}>
+          {error && (
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          )}
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
-        )}
-        <div className="mb-3">
-          <label htmlFor="username" className="form-label">
-            Username:
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+        <div className="mt-3 text-center">
+          New user? <Link to="/register">Create an account</Link>
         </div>
-        <div className="mb-3">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
-      <div className="mb-3 mt-3">
-        New user? <Link to="/register">Create an account</Link>
       </div>
     </div>
   );
--- song-front/src/components/RegisterForm.jsx
+++ song-front/src/components/RegisterForm.jsx
@@ -1,39 +1,33 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { register } from "../services/AuthService";
+import "../styles/Form.css";
 
 // Component for handling the user registration form
 const RegisterForm = () => {
-  // State hooks for form fields
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [lastName, setLastName] = useState("");
   const [firstName, setFirstName] = useState("");
 
-  // Hook for navigation
   const navigate = useNavigate();
 
-  // Handle form submission
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent default form submission behavior
+    e.preventDefault();
     try {
-      // Call the registration service function with form data
       const response = await register({
         username,
         password,
         firstName,
         lastName,
       });
-      console.log("Registration successful:", response);
+
       if (response.status === 200) {
-        // On successful register, save the token to session storage
         const { token } = response.data;
         sessionStorage.setItem("token", token);
-        console.log("Token saved:", sessionStorage.getItem("token")); // Log the saved token
-        console.log("Attempting to navigate to /users");
-        navigate("/home"); // Redirect to the user list page upon successful login
+        navigate("/home");
       } else {
-        console.error("Login failed", response.status);
+        console.error("Registration failed", response.status);
       }
     } catch (error) {
       console.error("Registration failed:", error);
@@ -41,61 +35,70 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className="container mt-5 w-25">
-      <h2 className="mb-4">Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div className="form-container">
+      <div className="card">
+        <h2 className="mb-4 text-center">Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="firstName" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Register
+          </button>
+        </form>
+        <div className="mt-3 text-center">
+          Have an account? <Link to="/">Log in now</Link>.
         </div>
-        <div className="mb-3">
-          <label htmlFor="firstName" className="form-label">
-            First Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="lastName" className="form-label">
-            Last Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
